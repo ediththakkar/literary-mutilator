05fe5cd59792a5e4db5da8c3a738b7539fbc058b
Edith Thakkar <egarcia262@cnm.edu>
2018-05-08 22:33:15 -0600

    added button type, paragraphs, and color change

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<script src="script.js"></script>
+		<title>Literary Mutilator</title>
+	</head>
+	<body style="color: darkmagenta">
+		<p>
+			Jelly macaroon bonbon pudding fruitcake halvah. Jujubes marzipan jelly powder cake. Candy canes cupcake marshmallow cookie
+			muffin chocolate bar sweet candy canes. Sweet danish jelly-o tart macaroon carrot cake. Chocolate bar jelly beans halvah
+			chupa chups fruitcake jelly beans sweet donut. Tootsie roll chupa chups bonbon chocolate cake.
+		</p>
+
+		<button type="button" onclick="highlightParagraphc2()">Highlight the paragraph:</button>
+		<p id="paragraph 2" style="font-style: italic">
+			Halvah cookie tiramisu powder lollipop jelly sugar plum powder. I love powder I love. Jujubes pie toffee lollipop
+			lemon drops sweet dessert I love. I love marshmallow cheesecake sugar plum. Donut pastry wafer cupcake marzipan.
+			Toffee cotton candy chocolate bar. Biscuit croissant marshmallow marzipan gummi bears. Tiramisu I love marzipan
+			danish macaroon lemon drops pie sweet roll. Topping cotton candy toffee I love sweet roll jelly beans I love dragée.
+			Cupcake I love biscuit oat cake jelly chocolate cotton candy.
+		</p>
+
+		<p id="paragraphc3" style onclick="changeTextColor()">
+			Soufflé candy icing candy. Sweet pie cheesecake oat cake apple pie topping icing oat cake. Tart sesame snaps bear
+			claw chocolate bar liquorice I love cake cookie. Cake lemon drops tootsie roll candy cake apple pie pastry apple pie.
+			Topping tiramisu I love bear claw cookie liquorice muffin apple pie biscuit. Croissant cupcake candy cotton candy
+			halvah marshmallow lemon drops wafer. I love fruitcake caramels I love. Ice cream chupa chups wafer I love I love
+			dessert pudding chocolate bar. Tart candy chocolate cake icing gummi bears sweet lemon drops sugar plum liquorice.
+		</p>
+
+	</body>
+</html>
---
+++ script.js
@@ -0,0 +1,10 @@
+/**
+ * Changes the background color of a paragraph
+ **/
+function changeBackgroundColor() {
+	document.getElementById("p3").style.backgroundColor = "red";
+}
+
+function changeTextColor(){
+	document.getElementById("paragraph3").style.color = "purple";
+}
